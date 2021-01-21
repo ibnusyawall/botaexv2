d4c315e615b441be326cd8c9edae5ca4f3ba7322
ibnusyawall <website.isas.24@gmail.com>
2021-01-21 10:34:50 +0700

    ✨ push project

--- mongo.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://aexbot:aexbotdb@cluster0.eiwny.mongodb.net/aexbot?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
--- setup.sh
+++
@@ -1,2 +0,0 @@
-apt update
-apt install xorgproto libpixman libcairo libjpeg libjpeg-turbo libpng libpng-static librsvg librsvg-static libxau
